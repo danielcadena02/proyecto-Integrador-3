4a1a0169756c933f8ba1136aa92de59b337549fd
danielcadena02 <62720871+danielcadena02@users.noreply.github.com>
2023-05-12 07:17:45 -0500

    actualizacion register + vista btn rosado
    actualizacion register + vista btn rosado

--- backend/models/adminModel.js
+++ backend/models/adminModel.js
@@ -9,7 +9,7 @@ const adminSchema = new mongoose.Schema({
         type: String,
         required: true,
         unique: true
-      },
+    },
     password: {
         type: String,
         required: true
